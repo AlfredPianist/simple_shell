8da18c9aa943a428f66cc7a1040f33c86ffd81f6
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-04-08 00:50:28 -0500

    Fix: test_env_main functional.
    TODO: implement env as builtin and also setenv. Continue working with PATH,
    possible pending work to better own parsing function.

--- test_env_main.c
+++ test_env_main.c
@@ -6,95 +6,94 @@
 #include <stdlib.h>
 
 void printenv(char **env);
-int _setenv(char **env, char *varN, char* varV, int overwrite);
+int _setenv(char ***env, char *varN, char *varV);
 char **copyEnv(char **env);
 
-int main(int ac, char **av, char **env_org)
+int main(__attribute__ ((unused)) int ac,
+	 __attribute__ ((unused)) char **av,
+	 char **env_org)
 {
-        int i = 0;
+	int i = 0;
+	char **env;
 
-	printf("copying env:\n");
-        char **env = copyEnv(env_org);
+	/* printf("copying env\n"); */
+	env = copyEnv(env_org);
+	/* printf("copied env\n"); */
 
-        // printenv(env);
-        _setenv(env, "NEWVAR1", "asdasdojieijoijowejiewrwer", 1);
-        //_setenv(env, "NEWVAR2", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR3", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR4", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR5", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR6", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR7", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR8", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR9", "asdasdojieijoijowejiewrwer", 1);
-        // _setenv(env, "NEWVAR10", "asdasdojieijoijowejiewrwer", 1);
-        printenv(env);
-	printf("freeing copy env:\n");
-        while(env[i])
-        {
-               // printf("%p => %s\n", env[i], env[i]);
-                free(env[i]), i++;
-        }
+	/* printf("printing original env\n"); */
+	/* printenv(env); */
+	/* printf("printing copied env\n"); */
+	/* printenv(env_org); */
+	/* printf("printed env\n"); */
 
-        free(env);
+	/* printf("setting new env variable\n"); */
+	_setenv(&env, "NEWVAR1", "asdasdojieijoijowejiewrwer");
+	/* printf("new variable set\n"); */
 
-        return (0);
+	/* printf("printing copied env with new variable\n"); */
+	printenv(env);
+	/* printf("printed copied env with new variable\n"); */
+
+	/* printf("freeing copy env:\n"); */
+	while (env[i])
+		free(env[i]), i++;
+	free(env);
+	/* printf("freed copy env\n"); */
+
+	return (0);
 }
 
 char **copyEnv(char **env)
 {
-        unsigned int i = 0, j=0;
-        char **newEnv;
+	unsigned int i = 0, j = 0;
+	char **newEnv;
 
-        while(env[i])
-                i++;
-        
-        newEnv = malloc(sizeof(char*) * (i + 1));
+	while (env[i])
+		i++;
 
-        for(i = 0; env[i]; i++)
-        {
-                newEnv[i] = malloc(sizeof(char) * strlen(env[i]));
-                
-                // for (j = 0; j < strlen(env[i]); j++)
-                //         newEnv[i][j] = env[i][j];
-                // newEnv[i][j] = 0;
-                memcpy(newEnv[i], env[i], strlen(env[i]));
-                
-               // printf("cpy=%p, org=%p\n", newEnv[i], env[i]);
-        }
+	newEnv = malloc(sizeof(*newEnv) * (i + 1));
 
-        newEnv[i] = NULL;
+	for (i = 0; env[i]; i++)
+	{
+		newEnv[i] = malloc(sizeof(*newEnv[i]) * (strlen(env[i]) + 1));
+		strcpy(newEnv[i], env[i]);
+		newEnv[i][strlen(env[i])] = '\0';
+	}
+	newEnv[i] = NULL;
 
-        return newEnv;
+	return (newEnv);
 }
 
-int _setenv(char **env, char *varN, char* varV, int overwrite)
+int _setenv(char ***env, char *varN, char *varV)
 {
-        int i;
-        int buff_size = strlen(varN) + strlen(varV) + 2;
-        char *buff = calloc(buff_size, sizeof(char));
-        
-        strcat(buff, varN);
-        strcat(buff, "=");
-        strcat(buff, varV);
-        buff[buff_size - 1] = 0;
+	int i;
+	int buff_size = strlen(varN) + strlen(varV) + 2;
+	char *buff = calloc(buff_size, sizeof(*buff));
 
-        for (i = 0; env[i]; i++)
-                if (strncmp(env[i], varN, strlen(varN)) == 0)
-                {
-                        env[i] = buff;
-                        return (1);
-                }
-        env = realloc(env, sizeof(char *) * (i + 2));
+	strcat(buff, varN);
+	strcat(buff, "=");
+	strcat(buff, varV);
+	buff[buff_size - 1] = '\0';
 
-        env[i] = buff;
-        env[i + 1] = NULL;
-        return (1);
+	for (i = 0; (*env)[i] != NULL; i++)
+	{
+		if (strncmp((*env)[i], varN, strlen(varN)) == 0)
+		{
+			(*env)[i] = buff;
+			return (1);
+		}
+	}
+	*env = realloc(*env, sizeof(**env) * (i + 2));
+	(*env)[i] = buff;
+	(*env)[i + 1] = NULL;
+
+	return (1);
 }
 
 void printenv(char **env)
 {
-        int i = 0;
+	int i = 0;
 
-        while (env[i])
-                printf("%s\n", env[i++]);
+	while (env[i] != NULL)
+		printf("%s\n", env[i++]);
 }
