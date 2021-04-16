abb78575a420a526ad23b9141a2ca606eb161160
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-04-16 15:15:56 +0000

    fix coments case

--- main.c
+++ main.c
@@ -103,7 +103,8 @@ list_t *pre_parse(char *l, list_t **controls)
 
 	for (i = 0; l[i]; i++)
 		if (_strncmp(l + i, "&&", 2) == 0 || _strncmp(l + i, "||", 2) == 0
-			|| l[i] == ';' || l[i + 1] == 0 || _strncmp(l + i, " #", 2) == 0) 
+			|| l[i] == ';' || l[i + 1] == 0 || _strncmp(l + i, " #", 2) == 0
+			|| (l[i] == '#' && i == 0 )) 
 		{
 			string = _realloc(0, 0, sizeof(char) * (i - k + 1));
 			_strncat(string, l + k, i - k);
@@ -120,7 +121,7 @@ list_t *pre_parse(char *l, list_t **controls)
 				add_node(controls, -1, ";");
 
 			free(string);
-			if (_strncmp(l + i, " #", 2) == 0)
+			if (_strncmp(l + i, " #", 2) == 0 || (l[i] == '#' && i == 0 ))
 				break;
 		}
 	add_node(controls, -1, ";");
